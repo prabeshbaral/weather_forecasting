ef8cee0f78275dd22d18cbf624ea065f21942c9d
Prabesh Baral <prabeshbaral59@gmail.com>
2025-01-09 21:32:22 +0545

    adding staticfiles

--- weather_forecasting/urls.py
+++ weather_forecasting/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+url_patterns = [
     path('',include('weather_app.urls')),
     path("admin/", admin.site.urls),
+
 ]
+
+urlpatterns = static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + url_patterns
